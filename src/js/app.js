0320f79ccd2867ed5b0a6c64f40ac2db143d4a4a
Evaneta <eftimovaevaneta@gmail.com>
2023-02-06 21:54:11 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,18 @@
 import "../scss/app.scss";
-const R = require("ramda");
+//const R = require("ramda");
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  //const isEven = (n) => n % 2 === 0;
+  //const filteredArray = R.filter(isEven, numbersArray);
+
+  const newArr = numbersArray.filter(el => el % 2 === 0)
+  newArr.forEach(element => {
+    let li = document.createElement("li")
+    li.textContent = element
+    ul.appendChild(li)
+  });
 });
